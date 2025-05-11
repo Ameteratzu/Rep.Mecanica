15016d1c2b3cd2a4391ec64eb6378b109fa9299f
Amet Marcelo <quispeamet2@gmail.com>
2025-05-10 23:15:14 -0500

    models.ṕy

--- my_flask_app/my_flask_app/my_flask_app/app/models.py
+++ my_flask_app/my_flask_app/my_flask_app/app/models.py
@@ -1,1 +1,12 @@
-# Aquí se definen los modelos.
+# Aquï¿½ se definen los modelos.
+# models.py
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Producto(db.Model):
+    __tablename__ = 'productos'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
