a948f5e1d5f0038d7ae95a8dcf1cf0019fefb3be
Amet Marcelo <125571134+Ameteratzu@users.noreply.github.com>
2025-07-08 08:58:05 -0500

    se agrego permisos y persona.py

---
+++ .history/app/models/__init___20250708085731.py
@@ -0,0 +1,38 @@
+from app.extensions import db
+
+from .usuario       import User
+from .cliente       import Cliente
+from .automovil     import Automovil
+from .categoria     import Categoria
+from .producto      import Producto
+from .servicio      import Servicio
+from .persona       import Persona
+from .proveedor     import Proveedor
+from .producto_proveedor import ProductoProveedor
+from .ingreso import Ingreso
+from .ingreso_producto import IngresoProducto
+from .inventario import Inventario
+from .venta import Venta
+from .cotizacion_estado import CotizacionEstado
+from .venta_producto import VentaProducto
+from .venta_servicio import VentaServicio
+
+__all__ = [
+    "db",
+    "User",
+    "Cliente",
+    "Automovil",
+    "Categoria",
+    "Producto",
+    "Servicio",
+    "Persona",
+    "Proveedor",
+    "ProductoProveedor",
+    "Ingreso",
+    "IngresoProducto",
+    "Inventario",
+    "Venta",
+    "CotizacionEstado",
+    "VentaProducto",
+    "VentaServicio"
+]
